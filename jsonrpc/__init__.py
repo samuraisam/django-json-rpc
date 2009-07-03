ed0e7be511bac3775c082ee33a58eb864687e21b
Samuel Sutch <samuraiblog@gmail.com>
2009-07-03 14:22:30 -0700

    Added setup file and license

---
+++ jsonrpc/__init__.py
@@ -0,0 +1,1 @@
+from site import jsonrpc_method
---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+  name="django-json-rpc",
+  version="0.1",
+  description="A simple JSON-RPC implementation for Django",
+  author="Samuel Sutch",
+  author_email="samuraiblog@gmail.com",
+  license="MIT",
+  url="http://github.com/samuraisam/django-json-rpc/tree/master",
+  download_url="http://github.com/samuraisam/django-json-rpc/tree/master",
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Intented Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independant',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'],
+  packages=['jsonrpc'])
