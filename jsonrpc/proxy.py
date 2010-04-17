2beaf8338c351c4d8c45001f1c975698b5c19589
Samuel Sutch <samuraiblog@gmail.com>
2010-04-16 22:03:27 -0700

    print jsonrpc error when DEBUG is on

--- jsonrpc/proxy.py
+++ jsonrpc/proxy.py
@@ -1,8 +1,7 @@
-import urllib
-import uuid
+import urllib, uuid
 from jsonrpc._json import loads, dumps
 from jsonrpc.types import *
-        
+
 class ServiceProxy(object):
   def __init__(self, service_url, service_name=None, version='1.0'):
     self.__version = str(version)
@@ -24,10 +23,19 @@ class ServiceProxy(object):
       raise Exception('Unsupport arg type for JSON-RPC 1.0 '
                      '(the default version for this client, '
                      'pass version="2.0" to use keyword arguments)')
+    # req = urllib2.Request(self.__service_url, )
     r = urllib.urlopen(self.__service_url,
                         dumps({
                           "jsonrpc": self.__version,
                           "method": self.__service_name,
                           'params': params,
                           'id': str(uuid.uuid1())})).read()
-    return loads(r)
+    y = loads(r)
+    if u'error' in y:
+      try:
+        from django.conf import settings
+        if settings.DEBUG:
+            print '%s error %r' % (self.__service_name, y)
+      except:
+        pass
+    return y
