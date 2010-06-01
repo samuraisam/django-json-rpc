699856b4ca416baa46f6bc18bfbd4cdf7879dc61
Sebastjan Trepča <trepca@gmail.com>
2010-06-01 21:48:06 +0800

    fixed version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
   name="django-json-rpc",
-  version="0.6.1",
+  version="0.6.2",
   description="A simple JSON-RPC implementation for Django",
   long_description="""
 Features:
